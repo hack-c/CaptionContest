42a60d593582cecc55b26aeaafdcd2d62be1df31
hack-c <charlie@205consulting.com>
2014-10-30 01:47:01 -0400

    read_csvs

--- caption_contest.py
+++ caption_contest.py
@@ -1,1 +1,30 @@
-caption_contest.py
+import os
+import sys
+import csv
+
+import numpy as np
+import pandas as pd
+
+
+def read_csvs():
+    """
+    read all csvs in data/raw/
+    return all rows
+    """
+
+    paths  = ['data/raw/' + f for f in os.listdir('data/raw/')]
+    rows   = []
+
+    for fp in paths:
+        with open(fp, 'rb') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                rows.append(row)
+
+    return rows
+
+
+
+if __name__ == "__main__":
+    rows = read_csvs()
+    
