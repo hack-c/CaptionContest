77eaa6553a7a2022a410ce5a75cf443dd61e258b
hack-c <charlie@205consulting.com>
2014-10-30 13:08:35 -0400

    missing import, minor bug

--- caption_contest.py
+++ caption_contest.py
@@ -1,9 +1,16 @@
 import os
 import sys
 import csv
+import string
+import nltk
 
 import numpy as np
 import pandas as pd
+
+
+nonascii_table = {i: None for i in range(128,65375)}
+punctuation_table = {ord(c): None for c in string.punctuation}
+
 
 
 def read_csvs():
@@ -24,7 +31,47 @@ def read_csvs():
     return rows
 
 
+def tokenize(raw_string):
+    """
+    take in a string, return a tokenized and normalized list of words
+    """
+    if raw_string == '':
+        return [u'']
+    if not isinstance(raw_string, unicode):
+        raise TypeError("%s is not unicode." % raw_string)
+    return filter(
+        lambda x: x not in nltk.corpus.stopwords.words('english'), 
+        nltk.word_tokenize(raw_string.lower().translate(punctuation_table))
+    )
+
+
+def remove_nonascii(s):
+    """
+    strip out nonascii chars
+    """
+    return s.translate(nonascii_table)
+
+
+def remove_uppercase(df):
+    """
+    drop every all-upper-case submission, because they universally suck
+    """
+    return df[df.CaptionText != df.CaptionText.apply(string.upper)]
+
+
 
 if __name__ == "__main__":
     rows = read_csvs()
-    
+    df = pd.DataFrame(rows)
+    df.columns = df.ix[0,:]
+    df = df[df.ContestID != 'ContestID']
+    df = remove_uppercase(df)
+    df['tokens'] = df.CaptionText.apply(remove_nonascii).apply(unicode).apply(tokenize)
+
+
+
+
+
+
+
+
